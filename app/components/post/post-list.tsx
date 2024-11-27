ed2b4e2f0df00cda2329fb71ee1da47605f13b5b
sebas-sala <sebastian_orko@hotmail.com>
2024-11-26 22:17:07 -0600

    Refactor PostList component: Integrate useInfinitePosts and usePostInteractions hooks for improved post loading and interaction handling

--- app/components/post/post-list.tsx
+++ app/components/post/post-list.tsx
@@ -1,75 +1,50 @@
-import { Loader } from "lucide-react";
-import { useInfiniteScroll } from "react-continous-scroll";
+import { Loader } from "~/components/loader";
+import { PostItem } from "~/components/post/post-item";
 
-import { PostItem } from "./post-item";
+import { useInfinitePosts } from "~/hooks/use-infinite-posts";
+import { usePostInteractions } from "~/hooks/use-post-interactions";
 
-import { useUserStore } from "~/store/user";
-import { usePostStore } from "~/store/post";
-
-import type { Post } from "~/types/post";
 import type { Pagination } from "~/types";
+import type { Post } from "~/types/post";
 
 interface Props {
   initialData: Post[];
   pagination: Pagination;
   fetchMore: (page: number) => Promise<Post[]>;
-  posts?: Post[];
-  maxPage?: number;
 }
 
-export function PostList({
-  posts,
-  initialData,
-  pagination,
-  fetchMore,
-  maxPage,
-}: Props) {
-  const { data, loadMoreRef, loading, loadMore } = useInfiniteScroll({
+export function PostList({ initialData, pagination, fetchMore }: Props) {
+  const { block, follow, like, unfollow, unlike } = usePostInteractions();
+
+  const { data, loadMore, loadMoreRef, loading } = useInfinitePosts({
     initialData,
-    loadMore: pagination?.hasNextPage,
-    onLoadMore: () => fetchMore(pagination.page + 1),
-    maxPage,
+    pagination,
+    fetchMore,
   });
-
-  const follow = useUserStore().follow;
-  const unfollow = useUserStore().unfollow;
-
-  const like = usePostStore().like;
-  const unlike = usePostStore().unlike;
 
   return (
     <>
-      <ul>
-        {posts && posts.length > 0
-          ? posts.map((post: Post) => (
-              <PostItem
-                key={post.id}
-                post={post}
-                follow={follow}
-                unfollow={unfollow}
-                like={like}
-                unlike={unlike}
-              />
-            ))
-          : data.map((post: Post) => (
-              <PostItem
-                key={post.id}
-                post={post}
-                follow={follow}
-                unfollow={unfollow}
-                like={like}
-                unlike={unlike}
-              />
-            ))}
-      </ul>
-      {!loading && loadMore && (
-        <div ref={loadMoreRef} className="my-10 flex justify-center">
-          {loading && <Loader size={32} className="animate-spin" />}
-          {!loading && !loadMore && (
-            <p className="text-gray-500">No more posts to load</p>
-          )}
-        </div>
+      {data && data.length > 0 ? (
+        <ul>
+          {data.map((post) => (
+            <PostItem
+              key={post.id}
+              post={post}
+              follow={follow}
+              unfollow={unfollow}
+              like={like}
+              unlike={unlike}
+              block={block}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-gray-500">No posts found</p>
       )}
+
+      <Loader ref={loadMoreRef} loading={loading} loadMore={loadMore}>
+        <p className="text-gray-500">No more posts to load</p>
+      </Loader>
     </>
   );
 }
