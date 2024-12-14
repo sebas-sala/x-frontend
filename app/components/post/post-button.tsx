952606106a3180b05f5a2d936ba0dd27e34f6498
Sebastian Sala <sebastian_orko@hotmail.com>
2024-12-14 13:43:09 -0600

    refactor PostButton component; remove unused media query and drawer logic

--- app/components/post/post-button.tsx
+++ app/components/post/post-button.tsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 
 import { PostDialog } from "./post-dialog";
-import { PostDrawer } from "./post-drawer";
-
-import { useMediaQuery } from "~/hooks/use-media-query";
 
 export function PostButton() {
   const [open, setOpen] = useState(false);
 
-  const isDesktop = useMediaQuery({
-    mediaQuery: "(min-width: 768px)",
-  });
+  // const isDesktop = useMediaQuery({
+  //   mediaQuery: "(min-width: 768px)",
+  // });
 
   const handleOnOpenChange = (open: boolean) => {
     setOpen(open);
   };
 
-  if (isDesktop) {
-    return <PostDialog open={open} handleOnOpenChange={handleOnOpenChange} />;
-  }
+  // if (isDesktop) {
+  // }
+  return <PostDialog open={open} handleOnOpenChange={handleOnOpenChange} />;
 
-  return <PostDrawer open={open} handleOnOpenChange={handleOnOpenChange} />;
+  // return <PostDrawer open={open} handleOnOpenChange={handleOnOpenChange} />;
 }
